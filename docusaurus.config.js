9ceebc2e401863dceffe3b0166ea98c5303e9f02
ScaredRabbit <scaredrabbitnl@gmail.com>
2024-08-08 15:12:15 +0200

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -24,7 +24,7 @@ const config = {
   organizationName: 'ScaredsMods', // Usually your GitHub org/user name.
   projectName: 'scaredsmods.github.io', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use Hernationalization, you can use this field to set
