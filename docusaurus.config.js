ca060f2ea9820c292ebb3427a881a7339d4e2f47
ScaredRabbit <scaredrabbitnl@gmail.com>
2024-08-08 14:55:07 +0200

    Fix?

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -22,7 +22,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'ScaredsMods', // Usually your GitHub org/user name.
-  projectName: 'wiki', // Usually your repo name.
+  projectName: 'scaredsmods.github.io', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
