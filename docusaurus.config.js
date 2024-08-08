3ed80b59fbe2c8ed5067695fa774736c422e8a53
ScaredRabbit <scaredrabbitnl@gmail.com>
2024-08-08 15:14:55 +0200

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -17,7 +17,7 @@ const config = {
   url: 'https://scaredrabbitnl.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/wiki/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
